205060a7435c63f11bb9df237bd5369268799bd0
Natalia Woodson <nwoodson@slack-corp.com>
2023-01-24 12:48:52 -0500

    add custom class for choiceParams

---
+++ src/main/java/com/adq/jenkins/xmljobtodsl/dsl/strategies/custom/DSLRenderChoiceParamsStrategy.java
@@ -0,0 +1,48 @@
+package com.adq.jenkins.xmljobtodsl.dsl.strategies.custom;
+
+import com.adq.jenkins.xmljobtodsl.dsl.strategies.DSLParameterStrategy;
+import com.adq.jenkins.xmljobtodsl.parsers.PropertyDescriptor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DSLRenderChoiceParamsStrategy extends DSLParameterStrategy {
+
+    private final String name;
+
+    public DSLRenderChoiceParamsStrategy(PropertyDescriptor propertyDescriptor) {
+        super(propertyDescriptor);
+        this.name = "";
+    }
+
+    public DSLRenderChoiceParamsStrategy(int tabs, PropertyDescriptor propertyDescriptor, String name) {
+        super(propertyDescriptor);
+        this.name = name;
+    }
+
+    @Override
+    public String toDSL() {
+        return printValueAccordingOfItsType(((PropertyDescriptor) getDescriptor()).getValue());
+    }
+
+    @Override
+    public String printValueAccordingOfItsType(String value) {
+        if (value == null) {
+            return "\"\"";
+        }
+        if (value.isEmpty()) {
+            return "\"\"";
+        }
+
+        value = value.replaceAll("\\\\", "\\\\\\\\");
+        value = value.replaceAll("\\$", Matcher.quoteReplacement("\\$"));
+
+        if (value.contains("\n")) {
+            value = value.replaceAll(Pattern.quote("\"\"\""), Matcher.quoteReplacement("\\\"\\\"\\\""));
+            return "\"\"\"" + value + "\"\"\"";
+        } else {
+            value = value.replaceAll(Pattern.quote("\""), Matcher.quoteReplacement("\\\""));
+        }
+        return "\"" + value + "\"";
+    }
+}
